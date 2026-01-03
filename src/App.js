e89d25dd953a6f483ae6f7ec54ac4b8df6c9dc09
Ijlal <shaikhijlalahmed1998@gmail.com>
2026-01-03 15:47:51 +0500

    map through users and render themin list

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
-import logo from './logo.svg';
+
 import './App.css';
 import React from 'react';
 
 function App() {
 
   const [search, setSearch] = React.useState(''); //state for search input
+  const users = [
+  { id: 1, name: "Ali", email: "ali@test.com" },
+  { id: 2, name: "Sara", email: "sara@test.com" }  ]; //sample user data
+
 
 
   //JSX
   return (
     <div>
       <input type="text" value={search} onChange={(e) => setSearch(e.target.value)} />
-      <h1>{search}</h1>
+      //map through users and display them
+      <ul>
+        {users.map(user => (
+  <div key={user.id}>
+    {user.name} - {user.email}
+  </div>
+))}
+
+      </ul>
     </div>
   );
 }
